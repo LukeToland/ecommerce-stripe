21315289d2bd23a1b57f8a3f0e5a746a3f31ff0b
Luke Toland <luke-toland@hotmail.com>
2022-07-31 10:12:44 +0100

    Updates

---
+++ components/Navbar.jsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import React from "react";
+import { urlFor } from "../lib/client";
+import { AiOutlineShopping, AiFillHome } from "react-icons/ai";
+import { BiHeadphone } from "react-icons/bi";
+import { MdOutlinePointOfSale } from "react-icons/md";
+import Cart from "./Cart";
+import { useStateContext } from "../context/StateContext";
+
+const Navbar = () => {
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
+  return (
+    <>
+      <div className="navbar-container">
+        <BiHeadphone className="nav-logo" size={35} />
+        <p className="logo">
+          {/* <Link href="/">
+          <p className="logo-items">Home</p>
+        </Link>
+        <p> | </p> */}
+          <Link href="/">
+            <p className="logo-items title">PAXECO</p>
+          </Link>
+          {/* <p> | </p>
+        <Link href="/">
+          <p className="logo-items">Autumn Sale</p>
+        </Link> */}
+        </p>
+        <button
+          type="button"
+          className="cart-icon"
+          onClick={() => {
+            setShowCart(true);
+          }}
+        >
+          <AiOutlineShopping size={35} />
+          <span className="cart-item-qty">{totalQuantities}</span>
+        </button>
+      </div>
+
+      {showCart && <Cart />}
+    </>
+  );
+};
+
+export default Navbar;
